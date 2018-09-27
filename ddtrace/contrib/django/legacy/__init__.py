0d2bf7fde83e0b130ee0a4a18f51b95bae136d31
Luca Abbati <luca.abbati@datadoghq.com>
2018-09-27 11:41:39 +0200

    [django_legacy] Just playing with legacy app initialization

---
+++ ddtrace/contrib/django/legacy/__init__.py
@@ -0,0 +1,63 @@
+import logging
+from ..conf import settings
+from ..middleware import insert_trace_middleware, insert_exception_middleware
+from ....ext import AppTypes
+from ..templates import patch_template
+from ..db import patch_db
+from ..cache import patch_cache
+
+log = logging.getLogger(__name__)
+
+tracer = settings.TRACER
+
+if settings.TAGS:
+    tracer.set_tags(settings.TAGS)
+
+# configure the tracer instance
+# TODO[manu]: we may use configure() but because it creates a new
+# AgentWriter, it breaks all tests. The configure() behavior must
+# be changed to use it in this integration
+tracer.enabled = settings.ENABLED
+tracer.writer.api.hostname = settings.AGENT_HOSTNAME
+tracer.writer.api.port = settings.AGENT_PORT
+
+# define the service details
+tracer.set_service_info(
+    app='django',
+    app_type=AppTypes.web,
+    service=settings.DEFAULT_SERVICE,
+)
+
+if settings.AUTO_INSTRUMENT:
+    log.info("Instrumenting Django legacy app")
+    # trace Django internals
+    insert_trace_middleware()
+    insert_exception_middleware()
+
+    if settings.INSTRUMENT_TEMPLATE:
+        try:
+            patch_template(tracer)
+        except Exception:
+            log.exception('error patching Django template rendering')
+
+    if settings.INSTRUMENT_DATABASE:
+        try:
+            patch_db(tracer)
+        except Exception:
+            log.exception('error patching Django database connections')
+
+    if settings.INSTRUMENT_CACHE:
+        try:
+            patch_cache(tracer)
+        except Exception:
+            log.exception('error patching Django cache')
+
+    # # Instrument rest_framework app to trace custom exception handling.
+    # if apps.is_installed('rest_framework'):
+    #     try:
+    #         from .restframework import patch_restframework
+    #         patch_restframework(tracer)
+    #     except Exception:
+    #         log.exception('error patching rest_framework app')
+else:
+    log.info("Django instrumenting was disabled")
