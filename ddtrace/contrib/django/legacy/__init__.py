ff3b64cf766a0613d033e6c65bae51f1947eca6c
kyle-verhoog <kyle@verhoog.ca>
2018-10-01 18:54:12 +0200

    Get middleware tests working

--- ddtrace/contrib/django/legacy/__init__.py
+++ ddtrace/contrib/django/legacy/__init__.py
@@ -8,48 +8,56 @@ from ..cache import patch_cache
 
 log = logging.getLogger(__name__)
 
-tracer = settings.TRACER
 
-if settings.TAGS:
-    tracer.set_tags(settings.TAGS)
+def _ready():
+    """
+    The equivalent of contrib.django.TracerConfig.ready() for older versions
+    of Django where AppConfigs are not supported.
+    """
+    tracer = settings.TRACER
 
-# configure the tracer instance
-# TODO[manu]: we may use configure() but because it creates a new
-# AgentWriter, it breaks all tests. The configure() behavior must
-# be changed to use it in this integration
-tracer.enabled = settings.ENABLED
-tracer.writer.api.hostname = settings.AGENT_HOSTNAME
-tracer.writer.api.port = settings.AGENT_PORT
+    if settings.TAGS:
+        tracer.set_tags(settings.TAGS)
 
-# define the service details
-tracer.set_service_info(
-    app='django',
-    app_type=AppTypes.web,
-    service=settings.DEFAULT_SERVICE,
-)
+    # configure the tracer instance
+    # TODO[manu]: we may use configure() but because it creates a new
+    # AgentWriter, it breaks all tests. The configure() behavior must
+    # be changed to use it in this integration
+    tracer.enabled = settings.ENABLED
+    tracer.writer.api.hostname = settings.AGENT_HOSTNAME
+    tracer.writer.api.port = settings.AGENT_PORT
 
-if settings.AUTO_INSTRUMENT:
-    log.info("Instrumenting Django legacy app")
-    # trace Django internals
-    insert_trace_middleware()
-    insert_exception_middleware()
+    # define the service details
+    tracer.set_service_info(
+        app='django',
+        app_type=AppTypes.web,
+        service=settings.DEFAULT_SERVICE,
+    )
 
-    if settings.INSTRUMENT_TEMPLATE:
-        try:
-            patch_template(tracer)
-        except Exception:
-            log.exception('error patching Django template rendering')
+    if settings.AUTO_INSTRUMENT:
+        log.info("Instrumenting Django legacy app")
+        # trace Django internals
+        insert_trace_middleware()
+        insert_exception_middleware()
 
-    if settings.INSTRUMENT_DATABASE:
-        try:
-            patch_db(tracer)
-        except Exception:
-            log.exception('error patching Django database connections')
+        if settings.INSTRUMENT_TEMPLATE:
+            try:
+                patch_template(tracer)
+            except Exception:
+                log.exception('error patching Django template rendering')
 
-    if settings.INSTRUMENT_CACHE:
-        try:
-            patch_cache(tracer)
-        except Exception:
-            log.exception('error patching Django cache')
-else:
-    log.info("Django instrumenting was disabled")
+        if settings.INSTRUMENT_DATABASE:
+            try:
+                patch_db(tracer)
+            except Exception:
+                log.exception('error patching Django database connections')
+
+        if settings.INSTRUMENT_CACHE:
+            try:
+                patch_cache(tracer)
+            except Exception:
+                log.exception('error patching Django cache')
+    else:
+        log.info("Django instrumenting was disabled")
+
+_ready()
